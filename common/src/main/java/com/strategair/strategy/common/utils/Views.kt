7a635b660e95c4c8e18f29d01d021d68b2552a7b
tanwenkang <phantomvk@163.com>
2020-02-14 19:33:51 +0800

    Mask.

--- common/src/main/java/com/strategair/strategy/common/utils/Views.kt
+++ common/src/main/java/com/strategair/strategy/common/utils/Views.kt
@@ -1,10 +1,16 @@
 package com.strategair.strategy.common.utils
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.PixelFormat
+import android.os.IBinder
 import android.text.TextUtils
+import android.view.View
+import android.view.WindowManager.LayoutParams
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.appcompat.widget.Toolbar
 
 fun Toolbar.setTitleEllipsize(where: TextUtils.TruncateAt) {
@@ -19,4 +25,19 @@ fun Toolbar.setTitleEllipsize(where: TextUtils.TruncateAt) {
 fun EditText.showSoftInput() {
     (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
         .showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun Activity.addMask(token: IBinder, @ColorInt color: Int): View {
+    val p = LayoutParams()
+    p.width = LayoutParams.MATCH_PARENT
+    p.height = LayoutParams.MATCH_PARENT
+    p.type = LayoutParams.TYPE_APPLICATION_PANEL
+    p.format = PixelFormat.TRANSLUCENT
+    p.token = token
+
+    val v = View(this)
+    v.setBackgroundColor(color)
+    v.fitsSystemWindows = false
+    windowManager.addView(v, p)
+    return v
 }
